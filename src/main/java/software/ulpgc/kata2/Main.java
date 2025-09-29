d75ede74446bec1c397179c857586371b51f2246
Entrar <entrar@entrar.entrar>
2025-09-29 20:09:03 +0100

    input stream

--- src/main/java/software/ulpgc/kata2/Main.java
+++ src/main/java/software/ulpgc/kata2/Main.java
@@ -1,12 +1,19 @@
 package software.ulpgc.kata2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
 
 public class Main {
-    public static void main(String[] args) {
-        String s = "España";
-        byte[] bytes = s.getBytes(StandardCharsets.ISO_8859_1);
-        //System.out.println("longitud de la string =" + s.length());
-        //System.out.println("longitud del array =" + bytes.length);
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://datasets.imdbws.com/title.basics.tsv.gz");
+        URLConnection connection = url.openConnection();
+        connection.connect();
+        InputStream is1 = connection.getInputStream();
+        GZIPInputStream is2 = new GZIPInputStream(is1)
     }
 }
